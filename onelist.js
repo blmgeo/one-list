db57ef3151d393db350afe904ff298ad9d665e3f
Brandon Mailhiot <brandonmailhiot@Brandons-MacBook-Air.local>
2017-07-15 03:14:51 -0700

    linted with airbnb styleguide

--- onelist.js
+++ onelist.js
@@ -1,22 +1,21 @@
-const scraper = require('scraperjs'),
-    fs = require('fs'),
-    fileToWrite = 'one-list.json',
-    url = 'https://en.wikipedia.org',
-    linkSelector = '.mw-content-ltr ul li a',
-    scrape = (page, func) => scraper.StaticScraper.create(url + page).scrape(func),
-    writeFile = res => {
-        fs.appendFile(fileToWrite, JSON.stringify(res, null, '\t'))
-    }
+const scraper = require('scraperjs');
+const fs = require('fs');
 
-scrape('/wiki/List_of_lists_of_lists', function($) {
-    function getText() {
-        const title = $(this).attr('title')
-        scrape($(this).attr('href'), function() {
-            const list = $(linkSelector).map(function() {
-                return {title: $(this).attr('title'), href: $(this).attr('href')}
-            }).get()
-            return {title, list}
-        }).then(writeFile)
-    }
-    $(linkSelector).map(getText).get()
-})
+const fileToWrite = 'one-list.json';
+const url = 'https://en.wikipedia.org';
+const linkSelector = '.mw-content-ltr ul li a';
+const scrape = (page, func) => scraper.StaticScraper.create(url + page).scrape(func);
+const writeFile = res => fs.appendFile(fileToWrite, JSON.stringify(res, null, '\t'));
+
+scrape('/wiki/List_of_lists_of_lists', ($) => {
+  function getText() {
+    const title = $(this).attr('title');
+    scrape($(this).attr('href'), () => {
+      const list = $(linkSelector).map(function listObj() {
+        return { title: $(this).attr('title'), href: $(this).attr('href') };
+      }).get();
+      return { title, list };
+    }).then(writeFile);
+  }
+  $(linkSelector).map(getText).get();
+});
