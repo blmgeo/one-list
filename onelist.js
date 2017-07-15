774e334841c0748f3f2aff62012cf7952d8adab5
Brandon Mailhiot <brandonmailhiot@Brandons-MacBook-Air.local>
2017-07-14 18:51:24 -0700

    refactored

--- onelist.js
+++ onelist.js
@@ -1,26 +1,22 @@
 const scraper = require('scraperjs'),
-      fs = require('fs'),
-      fileToWrite = 'one-list.json',
-      url = 'https://en.wikipedia.org',
-      linkSelector = '.mw-content-ltr ul li a',
-      scrape = (page, func) => scraper.StaticScraper.create(url + page).scrape(func)
+    fs = require('fs'),
+    fileToWrite = 'one-list.json',
+    url = 'https://en.wikipedia.org',
+    linkSelector = '.mw-content-ltr ul li a',
+    scrape = (page, func) => scraper.StaticScraper.create(url + page).scrape(func),
+    writeFile = res => {
+        fs.appendFile(fileToWrite, JSON.stringify(res, null, '\t'))
+    }
 
-function writeFile (res) {
-    fs.appendFile(fileToWrite, JSON.stringify(res, null, '\t'))
-}
-
-function getPage ($) {
+scrape('/wiki/List_of_lists_of_lists', function($) {
     function getText() {
-        const title = () => $(this).attr('title'),
-              href  = () => $(this).attr('href'),
-              listObj = () => ({ title: title(), href: href() }),
-              getSubPage = $ => {
-                  const list = $(linkSelector).map(listObj).get()
-                  return { title: title(), list }
-              }
-        scrape(href(), getSubPage).then(writeFile)
+        const title = $(this).attr('title')
+        scrape($(this).attr('href'), function() {
+            const list = $(linkSelector).map(function() {
+                return {title: $(this).attr('title'), href: $(this).attr('href')}
+            }).get()
+            return {title, list}
+        }).then(writeFile)
     }
     $(linkSelector).map(getText).get()
-}
-
-scrape('/wiki/List_of_lists_of_lists', getPage)
+})
